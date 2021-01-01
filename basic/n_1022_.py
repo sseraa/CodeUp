9d7acddd53e681d7ebbd5b013182346e32939858
sseraa <gustjs0163@gmail.com>
2021-01-02 00:24:47 +0900

    Update n_1022_.py
    add 23,24

--- basic/n_1022_.py
+++ basic/n_1022_.py
@@ -3,3 +3,15 @@
 string = input()
 print(string)
 
+#1023
+# map(a,b)
+#f,b = input().split(".") #what diff?
+f,b = map(str,input().split("."))
+print(f,b,sep="\n")
+print(type(f))
+
+#1024
+string = input()
+for i in range(len(string)):
+  print("'"+string[i]+"'")
+

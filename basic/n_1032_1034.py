2780d175985025b36fef72e2ea1a6395d7b4beb1
sseraa <gustjs0163@gmail.com>
2021-01-07 00:36:10 +0900

    Create n_1032_1034.py
    integer <--> oct, hex

---
+++ basic/n_1032_1034.py
@@ -0,0 +1,17 @@
+#21.1.6
+
+#1032
+
+# a = int(input())
+# print(format(a,'x')
+
+a = int(input())
+print(hex(a)[2:])
+
+# #1033
+a = int(input())
+print(hex(a)[2:].upper())
+
+#1034
+a = input()
+print(int(a,8))
